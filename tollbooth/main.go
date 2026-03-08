625c1aa04edc0a58393aa561b20047f1427a0bbe
YugShrivastava <shrivastavayug04@gmail.com>
2026-03-08 09:27:49 +0530

    tollbooth method implemented

---
+++ tollbooth/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	
+	tollbooth "github.com/didip/tollbooth/v7"
+)
+
+type Message struct {
+	Status 	string `json:"status"`
+	Body 	string `json:"body"`
+}
+
+func endpointHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	message := Message{
+		Status: "Success",
+		Body: "You've successfully reached the api",
+	}
+	
+	if err := json.NewEncoder(w).Encode(message); err != nil {
+		return
+	}
+}
+
+func main() {
+	message := Message{
+		Status: "Request Failed",
+		Body: "The API is at capacity, try again later.",
+	}
+	
+	jsonMessage, _ := json.Marshal(message)
+	
+	tlbthLimiter := tollbooth.NewLimiter(1, nil)
+	tlbthLimiter.SetMessageContentType("application/json")
+	tlbthLimiter.SetMessage(string(jsonMessage))
+	
+	http.Handle("/ping", tollbooth.LimitFuncHandler(tlbthLimiter, endpointHandler))
+	log.Println("Server listening on port 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Println("Error in listening on port 8000:", err)
+	}
+}
